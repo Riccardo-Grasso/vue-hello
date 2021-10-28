8c89e4fde0e3a7280c5b9530ba52a0034af5b747
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-10-28 15:34:08 +0200

    v_1.0

--- js/script.js
+++ js/script.js
@@ -3,8 +3,11 @@ Vue.config.devtools = true;
 const app = new Vue({
     el: "#container",
     data: {
-        username: "Booleaners!"
-
+        username: "Booleaner!",
+        stiliH1: "text-color text-center my-5",
+        pic: "https://www.boolean.careers/images/misc/logo.png",
+        stiliDiv: "text-center",
+        stiliImg: "w-25"
     }
 
 });
